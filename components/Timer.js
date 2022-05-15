a662f3718bc6d0b62fcdf418fb2910ca646671c1
oboitsun <thewip3out@gmail.com>
2022-05-15 21:04:15 +0300

    new date

--- components/Timer.js
+++ components/Timer.js
@@ -3,7 +3,7 @@ import TimerCountItem from "./TimerCountItem";
 
 import { calculateTimeLeft } from "./utils";
 export default function Timer() {
-  const dayX = new Date("February 23, 2022 00:00:00").getTime();
+  const dayX = new Date("December 23, 2022 00:00:00").getTime();
   const [timeLeft, setTimeLeft] = useState({
     hours: "0",
     days: "0",
